6c50efe6e30f0b04bcb8a08570cfd2df94970ea0
Jean George <jean.george.alves@gmail.com>
2022-07-07 18:09:50 -0300

    new tests

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ mkdir $folder
 
 mix escript.build
 
+printf "\n\n"
 for test in $tests; do
+    printf "======================$test benchmark ======================\n"
+    printf "escript benchmarks $test 1 2> $folder/$test.txt\n"
     escript benchmarks $test 1 2> $folder/$test.txt
+    printf "======================$test benchmark ======================\n\n\n\n"
 done
