042787d459afcec5d78f8327a372e70939e6407f
Jean George <jean.george.alves@gmail.com>
2022-07-27 00:59:01 -0300

    adding keys position

--- lib/bhaskara.ex
+++ lib/bhaskara.ex
@@ -7,4 +7,4 @@ delta = b ** 2 - 4 * a * c
 x1 = Decimal.div(Decimal.add(Decimal.new(Decimal.negate(b)), Decimal.sqrt(delta)), Decimal.new(Decimal.mult(Decimal.new(2), Decimal.new(a))))
 x2 = Decimal.div(Decimal.sub(Decimal.new(Decimal.negate(b)), Decimal.sqrt(delta)), Decimal.new(Decimal.mult(Decimal.new(2), Decimal.new(a))))
 [Decimal.to_string(x1), Decimal.to_string(x2)]
-:erts_internal.debug_on()
+:erts_internal.debug_off()
